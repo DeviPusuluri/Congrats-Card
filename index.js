206530968bf56bf6ded343f18953d92f49f4fb11
DeviPusuluri <DeviPusuluri@_Example_ .com>
2023-10-19 09:35:38 +0000

    Congratscard

---
+++ index.js
@@ -0,0 +1,23 @@
+const element = (
+    // Write your code here.
+    <div>
+<img src="https://assets.ccbp.in/frontend/react-js/congrats-card-bg.png" 
+<div>
+  <h1 className="heading">Congratulations</h1>  
+  <div className="img-container"></div> 
+  <img className="image"
+  
+  src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+  />
+<h1 className="heading1">Kiran V</h1>
+<p className="paragraph">Vishnu Institute of computer Education and Technology</p>
+  <img className="image"
+src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+
+  />
+</div>
+/>
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
